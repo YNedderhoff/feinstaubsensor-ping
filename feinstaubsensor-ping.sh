95eaeabe2ce233b077ea49a4a0b7eb0925afba0f
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2017-06-07 20:01:03 +0200

    Add feinstaubsensor ping script

---
+++ feinstaubsensor-ping.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MAIL=$(awk '/^mail/{print $3}' feinstaubsensor-ping.conf)
+SENSOR=$(awk '/^feinstaubsensor/{print $3}' feinstaubsensor-ping.conf)
+LOGFILE=$(awk '/^logfile/{print $3}' feinstaubsensor-ping.conf)-"$(date +%Y%m%d)".log
+
+ping -c 1 $SENSOR >/dev/null 2>&1
+PING=$?
+
+SUBJECT="$SENSOR offline"
+FAILMESSAGE="$SENSOR seems to be offline. Status $PING."
+SUCCESSMESSAGE="$SENSOR ok, status $PING"
+
+if [ $PING -ne 0 ] ; then 
+    echo "$FAILMESSAGE" >> $LOGFILE
+    echo "$FAILMESSAGE" | mail -s "$SUBJECT" "$MAIL"
+else
+    echo "$SUCCESSMESSAGE" >> $LOGFILE
+fi
