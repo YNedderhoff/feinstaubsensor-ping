10b0d6c3957897c35d20bb736f706ace13c2397d
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2017-06-08 00:23:45 +0200

    add config param

--- feinstaubsensor-ping.sh
+++ feinstaubsensor-ping.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-MAIL=$(awk '/^mail/{print $3}' feinstaubsensor-ping.conf)
-SENSOR=$(awk '/^feinstaubsensor/{print $3}' feinstaubsensor-ping.conf)
-LOGFILE=$(awk '/^logfile/{print $3}' feinstaubsensor-ping.conf)-"$(date +%Y%m%d)".log
+CONF=$1
+MAIL=$(awk '/^mail/{print $3}' $CONF)
+SENSOR=$(awk '/^feinstaubsensor/{print $3}' $CONF)
+LOGFILE=$(awk '/^logfile/{print $3}' $CONF)-"$(date +%Y%m%d)".log
 
 ping -c 1 $SENSOR >/dev/null 2>&1
 PING=$?
